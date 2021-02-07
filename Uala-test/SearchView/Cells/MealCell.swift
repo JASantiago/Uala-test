4e4acfa38c002f6967f0b5e80d20425d7a9e28f1
Jose Santiago <josesantiluna@gmail.com>
2021-02-07 14:22:47 -0600

    Meal Cell Construction
    - Created the cell logic along with the Nib
    - Constructed a Image related cache helper
    - Created the ReausableView and NibLoadable protocols to make easier the conformance of the cell to the future tableView

---
+++ Uala-test/SearchView/Cells/MealCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealCell.swift
+//  Uala-test
+//
+//  Created by  Jose  Santiago on 7/2/21.
+//
+
+import UIKit
+
+class MealCell: UITableViewCell, ReusableView, NibLoadableView {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var mealImageView: UIImageView!
+
+    // MARK: - Methods
+
+    func configure(meal: Meal) {
+        titleLabel.text = meal.strMeal
+        categoryLabel.text = meal.strCategory
+        UIImageView().getImageFromCache(urlString: meal.strMealThumb) { [weak self] (image: UIImage?) in
+            DispatchQueue.main.async {
+                self?.mealImageView.image = image
+            }
+        }
+    }
+}
---
+++ Uala-test/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Uala-test
+//
+//  Created by  Jose  Santiago on 7/2/21.
+//
+
+import Foundation
+
+struct Constants {
+    struct ImageNames {
+        static let defaultImageName = "generic-meal"
+    }
+}
